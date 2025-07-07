1efe5d439700ab0afb12bf59989fb428831f8b0f
dogyun00 <kimdk0903@naver.com>
2025-07-07 19:48:43 +0900

    Initial commit

---
+++ src/test/java/com/lockers/outerpark/OuterparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockers.outerpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
