9c75d6738cc00f397afd26206e934c74dcedd857
geoson <balsohn@naver.com>
2025-07-08 17:07:35 +0900

    feat: Seat DTO 팩토리 메서드 구현

--- src/main/java/com/lockers/outerpark/domain/seat/dto/response/SeatsStatusResponse.java
+++ src/main/java/com/lockers/outerpark/domain/seat/dto/response/SeatsStatusResponse.java
@@ -2,6 +2,8 @@ package com.lockers.outerpark.domain.seat.dto.response;
 
 import java.util.List;
 
+import com.lockers.outerpark.domain.seat.entity.Seat;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +14,21 @@ public class SeatsStatusResponse {
 	private int totalSeats;
 	private int availableSeats;
 	private List<SeatResponse> seats;
+
+	public static SeatsStatusResponse of(Long concertId, List<Seat> seats) {
+		List<SeatResponse> seatResponses = seats.stream()
+			.map(SeatResponse::fromEntity)
+			.toList();
+
+		int availableSeats = (int)seats.stream()
+			.filter(seat -> !seat.getIsReserved())
+			.count();
+
+		return SeatsStatusResponse.builder()
+			.concertId(concertId)
+			.totalSeats(seats.size())
+			.availableSeats(availableSeats)
+			.seats(seatResponses)
+			.build();
+	}
 }
