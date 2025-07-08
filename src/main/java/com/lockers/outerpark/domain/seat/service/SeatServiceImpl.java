49eabdb1f25564c10c8eaa2d5dbf40b86d2eeb08
geoson <balsohn@naver.com>
2025-07-08 11:18:40 +0900

    feat: SeatService 필요 메서드 추가

--- src/main/java/com/lockers/outerpark/domain/seat/service/SeatServiceImpl.java
+++ src/main/java/com/lockers/outerpark/domain/seat/service/SeatServiceImpl.java
@@ -1,7 +1,43 @@
 package com.lockers.outerpark.domain.seat.service;
 
+import com.lockers.outerpark.domain.seat.dto.response.SeatResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeatServiceImpl implements SeatService {
+
+    @Override
+    public boolean isReservable(Long seatId) {
+        return false;
+    }
+
+    @Override
+    public void reserveSeat(Long seatId) {
+
+    }
+
+    @Override
+    public void cancelSeatReservation(Long seatId) {
+
+    }
+
+    @Override
+    public SeatResponse getSeat(Long seatId) {
+        return null;
+    }
+
+    @Override
+    public void createSeatsForConcert(Long concertId) {
+
+    }
+
+    @Override
+    public void createSeatsForConcert(Long concertId, int seatCount) {
+
+    }
+
+    @Override
+    public void deleteAllSeatsByConcert(Long concertId) {
+
+    }
 }
