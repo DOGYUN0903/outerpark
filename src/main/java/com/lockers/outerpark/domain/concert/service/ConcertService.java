afb509d64edaa215b38e18dba905a24710f61e84
kimyongjun0129 <kimyongjun0129@naver.com>
2025-07-08 16:34:05 +0900

    refactor : 파라미터 누락 추가

--- src/main/java/com/lockers/outerpark/domain/concert/service/ConcertService.java
+++ src/main/java/com/lockers/outerpark/domain/concert/service/ConcertService.java
@@ -5,17 +5,18 @@ import org.springframework.data.domain.Page;
 import com.lockers.outerpark.domain.concert.dto.FindConcertResponse;
 import com.lockers.outerpark.domain.concert.dto.RegisterConcertRequest;
 import com.lockers.outerpark.domain.concert.dto.RegisterConcertResponse;
+import com.lockers.outerpark.domain.concert.dto.UpdateConcertRequest;
 import com.lockers.outerpark.domain.concert.dto.UpdateConcertResponse;
 
 public interface ConcertService {
-	RegisterConcertResponse registerConcert(Long userId,
-		RegisterConcertRequest request);
+    RegisterConcertResponse registerConcert(Long userId,
+        RegisterConcertRequest request);
 
-	UpdateConcertResponse updateConcert(Long userId);
+    UpdateConcertResponse updateConcert(Long userId, UpdateConcertRequest request);
 
-	FindConcertResponse findConcert(Long concertId);
+    FindConcertResponse findConcert(Long concertId);
 
-	Page<FindConcertResponse> findConcerts();
+    Page<FindConcertResponse> findConcerts();
 
-	void deleteConcert(Long userId);
+    void deleteConcert(Long userId);
 }
