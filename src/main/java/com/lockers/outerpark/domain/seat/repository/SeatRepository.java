62abb6624be9c24caa106574c84f4f4efbfbebdd
geoson <balsohn@naver.com>
2025-07-08 13:28:37 +0900

    feat: SeatRepository 쿼리 메서드 구현

--- src/main/java/com/lockers/outerpark/domain/seat/repository/SeatRepository.java
+++ src/main/java/com/lockers/outerpark/domain/seat/repository/SeatRepository.java
@@ -1,7 +1,20 @@
 package com.lockers.outerpark.domain.seat.repository;
 
-import com.lockers.outerpark.domain.seat.entity.Seat;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.lockers.outerpark.domain.seat.entity.Seat;
+
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+	// 콘서트별 좌석 조회
+	List<Seat> findByConcertIdAndIsDeletedFalse(Long concertId);
+
+	// 콘서트별 좌석 개수
+	long countByConcertIdAndIsDeletedFalse(Long concertId);
+
+	// 콘서트의 모든 좌석 조회 (Soft Delete용)
+	List<Seat> findByConcertId(Long concertId);
+
 }
