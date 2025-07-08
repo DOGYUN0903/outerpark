61c9434f24dd2c6e0904b9cb6d6c27739c7ca02e
dogyun00 <kimdk0903@naver.com>
2025-07-08 09:05:46 +0900

    refactor: User 엔티티 필드에 nullable = false 제약 추가

--- src/main/java/com/lockers/outerpark/domain/user/entity/User.java
+++ src/main/java/com/lockers/outerpark/domain/user/entity/User.java
@@ -16,14 +16,23 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false)
     private String email;
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false)
     private String nickname;
+
+    @Column(nullable = false)
     private LocalDate birth; // 생년월일
+
+    @Column(nullable = false)
     private String password;
+
     private Long balance; // 보유 잔액
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole userRole;
 
     public User(String email, String nickname, LocalDate birth, String password, Long balance, UserRole userRole) {
