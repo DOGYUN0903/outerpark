65559de7395ba7e43c67584b160747299d49e35d
kimyongjun0129 <kimyongjun0129@naver.com>
2025-07-08 12:34:52 +0900

    feat: Concert 초기 설정 수정 @Service 어노테이션 추가

--- src/main/java/com/lockers/outerpark/domain/concert/service/ConcertServiceImpl.java
+++ src/main/java/com/lockers/outerpark/domain/concert/service/ConcertServiceImpl.java
@@ -1,12 +1,14 @@
 package com.lockers.outerpark.domain.concert.service;
 
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
 
 import com.lockers.outerpark.domain.concert.dto.FindConcertResponse;
 import com.lockers.outerpark.domain.concert.dto.RegisterConcertRequest;
 import com.lockers.outerpark.domain.concert.dto.RegisterConcertResponse;
 import com.lockers.outerpark.domain.concert.dto.UpdateConcertResponse;
 
+@Service
 public class ConcertServiceImpl implements ConcertService {
 	@Override
 	public RegisterConcertResponse registerConcert(Long userId, RegisterConcertRequest request) {
