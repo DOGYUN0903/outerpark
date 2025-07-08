d7009f54fbd5440c83f23e8f9baecc0a4f016c42
kimyongjun0129 <kimyongjun0129@naver.com>
2025-07-08 12:19:25 +0900

    feat: Concert 초기 설정 수정 Concert Entity User 필드 추가 ConcertService 파라미터 수정

--- src/main/java/com/lockers/outerpark/domain/concert/entity/Concert.java
+++ src/main/java/com/lockers/outerpark/domain/concert/entity/Concert.java
@@ -3,12 +3,16 @@ package com.lockers.outerpark.domain.concert.entity;
 import java.time.LocalDateTime;
 
 import com.lockers.outerpark.common.entity.BaseEntity;
+import com.lockers.outerpark.domain.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -19,6 +23,10 @@ public class Concert extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "writer_id")
+	private User writer;
 
 	@Column(nullable = false)
 	private String title;
--- src/main/java/com/lockers/outerpark/domain/concert/service/ConcertService.java
+++ src/main/java/com/lockers/outerpark/domain/concert/service/ConcertService.java
@@ -1,26 +1,21 @@
 package com.lockers.outerpark.domain.concert.service;
 
 import org.springframework.data.domain.Page;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lockers.outerpark.domain.concert.dto.FindConcertResponse;
 import com.lockers.outerpark.domain.concert.dto.RegisterConcertRequest;
 import com.lockers.outerpark.domain.concert.dto.RegisterConcertResponse;
 import com.lockers.outerpark.domain.concert.dto.UpdateConcertResponse;
 
-import jakarta.validation.Valid;
-
 public interface ConcertService {
-	RegisterConcertResponse registerConcert(@AuthenticationPrincipal Long userId,
-		@Valid @RequestBody RegisterConcertRequest request);
+	RegisterConcertResponse registerConcert(Long userId,
+		RegisterConcertRequest request);
 
-	UpdateConcertResponse updateConcert(@AuthenticationPrincipal Long userId);
+	UpdateConcertResponse updateConcert(Long userId);
 
-	FindConcertResponse findConcert(@RequestParam Long concertId);
+	FindConcertResponse findConcert(Long concertId);
 
 	Page<FindConcertResponse> findConcerts();
 
-	void deleteConcert(@AuthenticationPrincipal Long userId);
+	void deleteConcert(Long userId);
 }
