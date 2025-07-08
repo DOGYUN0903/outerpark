2b2fe347b5b6b884eb0bdaef142ae5036e82bf15
geoson <balsohn@naver.com>
2025-07-08 18:20:58 +0900

    refactor: Seat Entity Concert 임포ë트 수정

--- src/main/java/com/lockers/outerpark/domain/seat/entity/Seat.java
+++ src/main/java/com/lockers/outerpark/domain/seat/entity/Seat.java
@@ -1,6 +1,14 @@
 package com.lockers.outerpark.domain.seat.entity;
 
-import jakarta.persistence.*;
+import com.lockers.outerpark.domain.concert.entity.Concert;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,14 +17,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class Seat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "concert_id")
-    private Concert concert;
+	@ManyToOne
+	@JoinColumn(name = "concert_id")
+	private Concert concert;
 
-    private int seatNumber;
-    private Boolean isReserved = false;
+	private int seatNumber;
+	private Boolean isReserved = false;
 }
