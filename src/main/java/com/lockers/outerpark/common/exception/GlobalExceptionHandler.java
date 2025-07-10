d1341732e10b0d40b1961268fb9f23e1ed906b33
kimyongjun0129 <kimyongjun0129@naver.com>
2025-07-10 11:55:17 +0900

    refactor: 오류 메시지 의미에 맞게 변경

--- src/main/java/com/lockers/outerpark/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lockers/outerpark/common/exception/GlobalExceptionHandler.java
@@ -1,18 +1,16 @@
 package com.lockers.outerpark.common.exception;
 
-import com.lockers.outerpark.common.response.ApiResponse;
+import java.util.List;
+
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.lockers.outerpark.common.response.ApiResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,24 +19,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
         return ResponseEntity
-                .status(e.getErrorCode().getStatus()) // 예외에 정의된 상태 코드로 응답
-                .body(ApiResponse.error(e.getMessage())); // 공통 응답 포맷으로 메시지 반환
+            .status(e.getErrorCode().getStatus()) // 예외에 정의된 상태 코드로 응답
+            .body(ApiResponse.error(e.getMessage())); // 공통 응답 포맷으로 메시지 반환
     }
 
     // 3. @Valid로 DTO를 검증할 때 발생하는 예외 처리 (RequestBody 검증 실패)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ApiResponse<List<String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    protected ResponseEntity<ApiResponse<List<String>>> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e) {
 
         // 검증 실패한 메시지를 모두 모아서 리스트로 반환
         List<String> errors = e.getBindingResult()
-                .getAllErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
+            .getAllErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .toList();
 
         return ResponseEntity
-                .badRequest() // 400 Bad Request
-                .body(ApiResponse.error(String.join("||", errors))); // || 구분자로 묶어 반환
+            .badRequest() // 400 Bad Request
+            .body(ApiResponse.error(String.join("||", errors))); // || 구분자로 묶어 반환
     }
 
     // null 값을 입력하였을 경우 (예: {"username" : ,
@@ -46,7 +45,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ApiResponse<Object>> handleJsonParseError(HttpMessageNotReadableException e) {
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.error("입력값이 누락되었습니다. 확인해주세요."));
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error("입력 값을 다시 확인해주세요."));
     }
 }
