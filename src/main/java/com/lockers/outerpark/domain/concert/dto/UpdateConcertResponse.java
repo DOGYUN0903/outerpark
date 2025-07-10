2ed8c25f79199de9b736ec69c450b24661811b44
kimyongjun0129 <kimyongjun0129@naver.com>
2025-07-10 20:02:49 +0900

    fix: N+1 문제 해결

--- src/main/java/com/lockers/outerpark/domain/concert/dto/UpdateConcertResponse.java
+++ src/main/java/com/lockers/outerpark/domain/concert/dto/UpdateConcertResponse.java
@@ -2,15 +2,14 @@ package com.lockers.outerpark.domain.concert.dto;
 
 import java.time.LocalDateTime;
 
-import com.lockers.outerpark.common.dto.UserDto;
 import com.lockers.outerpark.domain.concert.entity.Concert;
 
-public record UpdateConcertResponse(String title, UserDto writer, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record UpdateConcertResponse(String title, Long writerId, LocalDateTime createdAt, LocalDateTime updatedAt) {
 
     public static UpdateConcertResponse of(Concert concert) {
         return new UpdateConcertResponse(
             concert.getTitle(),
-            UserDto.of(concert.getWriter()),
+            concert.getWriter().getId(),
             concert.getCreatedAt(),
             concert.getUpdatedAt()
         );
