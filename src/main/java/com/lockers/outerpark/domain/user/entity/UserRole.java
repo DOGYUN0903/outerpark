f2037b7d9e234db506d1e88fccd78ed0d20294c0
dogyun00 <kimdk0903@naver.com>
2025-07-10 10:10:10 +0900

    fix: 회원가입 시 UserRole을 클라이언트가 아닌 서버에서 강제 지정하도록 변경
    - 기존에는 SignupRequest에서 전달된 UserRole을 사용하여 유저를 생성함
    - 보안상 문제가 될 수 있어 서버에서 무조건 USER 권한으로 설정하도록 수정

--- src/main/java/com/lockers/outerpark/domain/user/entity/UserRole.java
+++ src/main/java/com/lockers/outerpark/domain/user/entity/UserRole.java
@@ -1,17 +1,13 @@
 package com.lockers.outerpark.domain.user.entity;
 
-import java.util.Arrays;
-
-import static com.lockers.outerpark.domain.user.exception.UserException.*;
-
 public enum UserRole {
     USER,
     ADMIN;
 
-    public static UserRole of(String role) {
-        return Arrays.stream(UserRole.values())
-                .filter(r -> r.name().equalsIgnoreCase(role))
-                .findFirst()
-                .orElseThrow(InvalidUserRoleException::new);
-    }
+    // public static UserRole of(String role) {
+    //     return Arrays.stream(UserRole.values())
+    //             .filter(r -> r.name().equalsIgnoreCase(role))
+    //             .findFirst()
+    //             .orElseThrow(InvalidUserRoleException::new);
+    // }
 }
