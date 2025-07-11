5e8c27cf0506ba7e7a252af16d78d6e5d3651f66
kimyongjun0129 <kimyongjun0129@naver.com>
2025-07-11 17:49:46 +0900

    refactor: 공연 id를 통해 예매 내용 조회 추가

--- src/main/java/com/lockers/outerpark/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/lockers/outerpark/domain/reservation/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.lockers.outerpark.domain.reservation.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -12,11 +13,13 @@ import com.lockers.outerpark.domain.reservation.entity.Reservation;
 import com.lockers.outerpark.domain.reservation.entity.ReservationStatus;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-	Optional<Reservation> findByIdAndStatus(Long id, ReservationStatus status);
+    Optional<Reservation> findByIdAndStatus(Long id, ReservationStatus status);
 
-	Optional<Reservation> findByIdAndStatusNot(Long reservationId, ReservationStatus reservationStatus);
+    Optional<Reservation> findByIdAndStatusNot(Long reservationId, ReservationStatus reservationStatus);
 
-	@EntityGraph(attributePaths = {"reservationSeats", "reservationSeats.seat", "concert"})
-	@Query("SELECT r FROM Reservation r WHERE r.user.id = :userId AND r.status = :status")
-	Page<Reservation> findAllByUserIdAndStatus(Long userId, ReservationStatus status, Pageable pageable);
+    @EntityGraph(attributePaths = {"reservationSeats", "reservationSeats.seat", "concert"})
+    @Query("SELECT r FROM Reservation r WHERE r.user.id = :userId AND r.status = :status")
+    Page<Reservation> findAllByUserIdAndStatus(Long userId, ReservationStatus status, Pageable pageable);
+
+    List<Reservation> findByConcertId(Long concertId);
 }
