ef34bef37f993d0a9b50e35e6d97789993ef7515
Zyooon <wcw7373@gmail.com>
2025-07-14 11:23:59 +0900

    refactor : 단순 response 객체 record 로 변경

--- src/main/java/com/lockers/outerpark/domain/auth/dto/response/SignupResponse.java
+++ src/main/java/com/lockers/outerpark/domain/auth/dto/response/SignupResponse.java
@@ -1,10 +1,4 @@
 package com.lockers.outerpark.domain.auth.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class SignupResponse {
-    private String token;
+public record SignupResponse(String token) {
 }
